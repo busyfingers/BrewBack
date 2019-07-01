86863f8cc95be656556a830bc1fd993901431147
busyfingers <n.malmqvist@gmail.com>
2019-07-01 14:29:45 +0200

    Add more type definitions and completed conversation of config.js

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,35 +1,35 @@
-import { ApplicationConfig } from '../types';
+import { DatabaseConfig, PoolConfig } from '../types';
+import * as fs from 'fs';
+import * as path from 'path';
 
-const fs = require('fs');
-const path = require('path');
-const types = require('../types');
+let poolConfig = {} as PoolConfig;
+let databaseConfig = {} as DatabaseConfig;
 
-let configValues = {} as ApplicationConfig;
-
-const lib = {
+module.exports = {
     setConfigValues: function() {
-        configValues = JSON.parse(fs.readFileSync(path.join(__dirname, './config.json')));
-        config.dbConnectionPool = {
-            poolSize: configValues.dbPoolSize,
-            getTimeout: configValues.dbPoolGetTimeout,
-            retryInterval: configValues.dbPoolRetryInterval
+        const values = JSON.parse(fs.readFileSync(path.join(__dirname, './config.json')).toString());
+        poolConfig.poolSize = values.dbPoolSize;
+        poolConfig.poolGetTimeout = values.dbPoolGetTimeout;
+        poolConfig.poolRetryInterval = values.dbPoolRetryInterval;
+        databaseConfig.server = values.dbServer;
+        databaseConfig.authentication = {
+            type: 'default',
+            options: {
+                userName: values.dbUser,
+                password: values.dbPass
+            }
+        };
+        databaseConfig.options = {
+            database: values.dbName,
+            useUTC: false
         };
     },
     getDatabaseConfig: function() {
         return {
-            server: configValues.dbServer,
-            authentication: {
-                type: 'default',
-                options: {
-                    userName: configValues.dbUser,
-                    password: configValues.dbPass
-                }
-            },
-            options: {
-                database: configValues.dbName,
-                useUTC: false
-            }
+            server: databaseConfig.server,
+            authentication: databaseConfig.authentication,
+            options: databaseConfig.options
         };
-    }
+    },
+    poolConfig
 };
-module.exports = lib;
--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,20 @@
-export type ApplicationConfig {
-    dbServer: string;
-    dbUser: string;
-    dbPass: string;
-    dbName: string;
-    dbPoolSize: number;
-    dbPoolGetTimeout: number;
-    dbPoolRetryInterval: number;
-}
+export type PoolConfig = {
+    poolSize: number;
+    poolGetTimeout: number;
+    poolRetryInterval: number;
+};
+
+export type DatabaseConfig = {
+    server: string;
+    authentication: {
+        type: string;
+        options: {
+            userName: string;
+            password: string;
+        };
+    };
+    options: {
+        database: string;
+        useUTC: boolean;
+    };
+};
